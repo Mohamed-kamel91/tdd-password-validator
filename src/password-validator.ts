49f6ee0ae669c69f514bbe96978ca80520e1c46c
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-11-25 05:11:46 +0200

    refactor: create helper method to improve code readibilty

--- src/password-validator.ts
+++ src/password-validator.ts
@@ -1,17 +1,54 @@
+type PasswordValidationError =
+  | "InvalidLength"
+  | "MissingUppercase"
+  | "MissingDigit";
+
+type PasswordValidationResult = {
+  valid: boolean;
+  errors: PasswordValidationError[];
+};
+
 export class PasswordValidator {
   static validatePassword(password: string) {
-    if (password.length < 5 || password.length > 15) {
-      return { valid: false, errors: ["InvalidLength"] };
+    const result: PasswordValidationResult = {
+      valid: true,
+      errors: [],
+    };
+
+    if (!this.isValidLength(password)) {
+      result.errors.push("InvalidLength");
     }
 
-    if (!/[A-Z]/.test(password)) {
-      return { valid: false, errors: ["MissingUppercase"] };
+    if (!this.containsUppercase(password)) {
+      result.errors.push("MissingUppercase");
     }
 
-    if (!/[0-9]/.test(password)) { 
-      return { valid: false, errors: ["MissingDigit"] };
+    if (!this.containsDigit(password)) {
+      result.errors.push("MissingDigit");
     }
 
-    return { valid: true, errors: [] };
+    if (this.hasErrors(result.errors)) {
+      result.valid = false;
+    }
+
+    return result;
+  }
+
+  private static isValidLength(password: string) {
+    return password.length >= 5 && password.length <= 15;
+  }
+
+  private static hasErrors(errors: PasswordValidationResult["errors"]) {
+    return errors.length > 0;
+  }
+
+  private static containsUppercase(password: string) {
+    const uppercaseRegex = /[A-Z]/;
+    return uppercaseRegex.test(password);
+  }
+
+  private static containsDigit(password: string) {
+    const digitRegex = /\d/;
+    return digitRegex.test(password);
   }
 }
